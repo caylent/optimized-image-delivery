f5e8fdf251e762ef90207a9a47383181a0e87348
Carlos Espin <carlos.espin@caylent.com>
2023-08-01 16:33:29 -0400

    feat: Initial commit

---
+++ test/optimized-image-delivery.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OptimizedImageDelivery from '../lib/optimized-image-delivery-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/optimized-image-delivery-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OptimizedImageDelivery.OptimizedImageDeliveryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
