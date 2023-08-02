ef7b93cb3cfd5f8b665fc0c54e0550acac266ce4
Carlos Espin <carlos.espin@caylent.com>
2023-08-02 17:14:45 -0400

    feat: CloufFront distribution

--- bin/optimized-image-delivery.ts
+++ bin/optimized-image-delivery.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { OptimizedImageDeliveryStack } from '../lib/optimized-image-delivery-stack';
+import {OptimizedImageDeliveryStack} from '../lib/optimized-image-delivery-stack';
 
 const app = new cdk.App();
+
+if (!process.env.APEX_DOMAIN || !process.env.SUBDOMAIN || !process.env.STACK_ID) {
+    console.error("You need to define 'APEX_DOMAIN', 'SUBDOMAIN' and 'STACK_ID' as env vars");
+    process.exit(1)
+}
+
 new OptimizedImageDeliveryStack(app, 'OptimizedImageDeliveryStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
+    apexDomain: process.env.APEX_DOMAIN,
+    subdomain: process.env.SUBDOMAIN,
+    stackId: process.env.STACK_ID
 });
--- lib/optimized-image-delivery-stack.ts
+++ lib/optimized-image-delivery-stack.ts
@@ -1,16 +1,147 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Duration, Fn} from 'aws-cdk-lib';
+import {
+    AllowedMethods,
+    BehaviorOptions,
+    CachedMethods,
+    CfnDistribution,
+    CfnOriginAccessControl,
+    Distribution,
+    HttpVersion,
+    IOrigin,
+    OriginBindConfig,
+    PriceClass,
+    SecurityPolicyProtocol,
+    SSLMethod,
+    ViewerProtocolPolicy
+} from 'aws-cdk-lib/aws-cloudfront';
+import {Construct} from 'constructs';
+import {AaaaRecord, ARecord, PublicHostedZone, RecordTarget} from "aws-cdk-lib/aws-route53";
+import {DnsValidatedCertificate} from "aws-cdk-lib/aws-certificatemanager";
+import {BlockPublicAccess, Bucket, HttpMethods} from "aws-cdk-lib/aws-s3";
+import {PolicyStatement} from "aws-cdk-lib/aws-iam";
+import {CloudFrontTarget} from "aws-cdk-lib/aws-route53-targets";
+
+export interface OptimizedImageDeliveryStackProps extends cdk.StackProps {
+    apexDomain: string;
+    subdomain: string;
+    stackId: string;
+}
 
 export class OptimizedImageDeliveryStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: OptimizedImageDeliveryStackProps) {
+        super(scope, id, props);
+        const {apexDomain, subdomain, stackId} = props;
 
-    // The code that defines your stack goes here
+        const imageBucket: Bucket = new Bucket(this, "ImagesBucket", {
+            bucketName: `optimizedimagedelivery-a79b75-${stackId}`,
+            cors: [
+                {
+                    allowedMethods: [HttpMethods.GET, HttpMethods.HEAD],
+                    allowedOrigins: ['*'],
+                    allowedHeaders: ['*'],
+                }
+            ],
+            versioned: false,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OptimizedImageDeliveryQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const zone = PublicHostedZone.fromLookup(this, "ApexDomain", {
+            domainName: apexDomain,
+        });
+
+        const siteCertificate = new DnsValidatedCertificate(this, "WebsiteCertificate", {
+            domainName: `${subdomain}.${apexDomain}`,
+            hostedZone: zone,
+            region: "us-east-1"  //standard for acm certs
+        });
+
+        const defaultBehaviorOptions: Pick<BehaviorOptions, "origin"> & Partial<BehaviorOptions> = {
+            origin: {
+                bind: () => ({} as OriginBindConfig)
+            } as IOrigin, // because the S3Origin construct doesn't support Origin Access Control yet
+        };
+
+        const distribution = new Distribution(this, "SiteDistribution", {
+            domainNames: [`${subdomain}.${apexDomain}`],
+            certificate: siteCertificate,
+            errorResponses: [
+                {
+                    ttl: Duration.seconds(10),
+                    httpStatus: 404,
+                    responseHttpStatus: 404,
+                    responsePagePath: "/index.html",
+                },
+                {
+                    ttl: Duration.seconds(10),
+                    httpStatus: 403,
+                    responseHttpStatus: 403,
+                    responsePagePath: "/index.html",
+                }
+            ],
+            comment: 'Optimized image delivery blog post',
+            priceClass: PriceClass.PRICE_CLASS_ALL,
+            httpVersion: HttpVersion.HTTP2_AND_3,
+            enableIpv6: true,
+            sslSupportMethod: SSLMethod.SNI,
+            minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+            defaultRootObject: "index.html",
+            defaultBehavior: {
+                ...defaultBehaviorOptions,
+                viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                compress: true,
+                allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
+                cachedMethods: CachedMethods.CACHE_GET_HEAD
+            },
+        });
+
+        const bucketPolicy = new PolicyStatement();
+        const {distributionId} = distribution;
+        bucketPolicy.addActions('s3:GetObject');
+        bucketPolicy.addServicePrincipal('cloudfront.amazonaws.com');
+        bucketPolicy.addResources(`${imageBucket.bucketArn}/*`);
+        bucketPolicy.addCondition('StringEquals', {
+            'AWS:SourceArn': Fn.sub("arn:aws:cloudfront::${AWS::AccountId}:distribution/${distributionId}", {
+                distributionId
+            })
+        });
+        imageBucket.addToResourcePolicy(bucketPolicy);
+
+        // Need to use L1 construct for Origin Access Control
+        const originAccessControl = new CfnOriginAccessControl(this, 'DefaultOriginAccessControl', {
+            originAccessControlConfig: {
+                name: `OptimizedImageDelivery-oac-${stackId}`,
+                originAccessControlOriginType: 's3',
+                signingBehavior: 'always',
+                signingProtocol: 'sigv4',
+            },
+        });
+        const cfnDistribution = distribution.node.defaultChild as CfnDistribution;
+        const distributionConfig = cfnDistribution.distributionConfig as any;
+        distributionConfig.origins = [
+            {
+                domainName: imageBucket.bucketRegionalDomainName,
+                id: 'image-bucket',
+                originAccessControlId: originAccessControl.attrId,
+                s3OriginConfig: {}
+            }
+        ];
+        distributionConfig.defaultCacheBehavior.targetOriginId = 'image-bucket';
+
+        const target = RecordTarget.fromAlias(new CloudFrontTarget(distribution));
+        new ARecord(this, 'ARecord', {
+            recordName: `${subdomain}.${apexDomain}`,
+            zone,
+            target,
+            ttl: Duration.hours(1),
+            comment: `Optimized image delivery - '${stackId}'`,
+        });
+        new AaaaRecord(this, 'AAAARecord', {
+            recordName: `${subdomain}.${apexDomain}`,
+            zone,
+            target,
+            ttl: Duration.hours(1),
+            comment: `Optimized image delivery - '${stackId}'`,
+        });
+    }
 }
