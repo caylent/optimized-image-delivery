f75aecb48969c94ee3b02133aa370a7e0c1189a1
Carlos Espin <carlos.espin@caylent.com>
2023-08-03 22:57:05 -0400

    chore: cleaned up a bit

--- lib/optimized-image-delivery-stack.ts
+++ lib/optimized-image-delivery-stack.ts
@@ -17,7 +17,7 @@ import {
     ViewerProtocolPolicy
 } from 'aws-cdk-lib/aws-cloudfront';
 import {Construct} from 'constructs';
-import {AaaaRecord, ARecord, PublicHostedZone, RecordTarget} from "aws-cdk-lib/aws-route53";
+import {AaaaRecord, ARecord, IHostedZone, PublicHostedZone, RecordTarget} from "aws-cdk-lib/aws-route53";
 import {DnsValidatedCertificate} from "aws-cdk-lib/aws-certificatemanager";
 import {BlockPublicAccess, Bucket, HttpMethods} from "aws-cdk-lib/aws-s3";
 import {CompositePrincipal, ManagedPolicy, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
@@ -30,33 +30,92 @@ import * as path from 'path';
 export interface OptimizedImageDeliveryStackProps extends cdk.StackProps {
     apexDomain: string;
     subdomain: string;
-    stackId: string;
+    stack: string;
 }
 
 export class OptimizedImageDeliveryStack extends cdk.Stack {
+    readonly apexDomain: string;
+    readonly subdomain: string;
+    readonly stack: string;
+
     constructor(scope: Construct, id: string, props: OptimizedImageDeliveryStackProps) {
         super(scope, id, props);
-        const {apexDomain, subdomain, stackId} = props;
+        this.apexDomain = props.apexDomain;
+        this.subdomain = props.subdomain;
+        this.stack = props.stack;
 
-        const imageBucket: Bucket = new Bucket(this, "ImagesBucket", {
-            bucketName: `optimizedimagedelivery-a79b75-${stackId}`,
-            cors: [
-                {
-                    allowedMethods: [HttpMethods.GET, HttpMethods.HEAD],
-                    allowedOrigins: ['*'],
-                    allowedHeaders: ['*'],
-                }
-            ],
-            versioned: false,
-            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-        });
+        const imagesBucket: Bucket = this.imagesBucket();
 
         const zone = PublicHostedZone.fromLookup(this, "ApexDomain", {
-            domainName: apexDomain,
+            domainName: this.apexDomain,
         });
+        const distribution = this.createCloudfrontDistribution(zone);
 
+        this.allowDistributionToReadS3Bucket(distribution, imagesBucket);
+
+        this.setS3BucketAsDefaultOrigin(distribution, imagesBucket);
+
+        this.createDnsRecordsForCloudfrontDistribution(distribution, zone);
+    }
+
+    private createDnsRecordsForCloudfrontDistribution(distribution: Distribution, zone: IHostedZone) {
+        const target = RecordTarget.fromAlias(new CloudFrontTarget(distribution));
+        new ARecord(this, 'ARecord', {
+            recordName: `${this.subdomain}.${this.apexDomain}`,
+            zone,
+            target,
+            ttl: Duration.hours(1),
+            comment: `Optimized image delivery - '${this.stack}'`,
+        });
+        new AaaaRecord(this, 'AAAARecord', {
+            recordName: `${this.subdomain}.${this.apexDomain}`,
+            zone,
+            target,
+            ttl: Duration.hours(1),
+            comment: `Optimized image delivery - '${this.stack}'`,
+        });
+    }
+
+    private setS3BucketAsDefaultOrigin(distribution: Distribution, imagesBucket: Bucket) {
+        // Need to use L1 construct for Origin Access Control
+        const originAccessControl = new CfnOriginAccessControl(this, 'DefaultOriginAccessControl', {
+            originAccessControlConfig: {
+                name: `OptimizedImageDelivery-oac-${this.stack}`,
+                originAccessControlOriginType: 's3',
+                signingBehavior: 'always',
+                signingProtocol: 'sigv4',
+            },
+        });
+        const cfnDistribution = distribution.node.defaultChild as CfnDistribution;
+        const distributionConfig = cfnDistribution.distributionConfig as any;
+        distributionConfig.origins = [
+            {
+                domainName: imagesBucket.bucketRegionalDomainName,
+                id: 'image-bucket',
+                originAccessControlId: originAccessControl.attrId,
+                s3OriginConfig: {}
+            }
+        ];
+        distributionConfig.defaultCacheBehavior.targetOriginId = 'image-bucket';
+    }
+
+    private allowDistributionToReadS3Bucket(distribution: Distribution, imagesBucket: Bucket) {
+        const bucketPolicy = new PolicyStatement();
+        const {distributionId} = distribution;
+        bucketPolicy.addActions('s3:GetObject');
+        bucketPolicy.addServicePrincipal('cloudfront.amazonaws.com');
+        bucketPolicy.addResources(`${imagesBucket.bucketArn}/*`);
+        bucketPolicy.addCondition('StringEquals', {
+            'AWS:SourceArn': Fn.sub("arn:aws:cloudfront::${AWS::AccountId}:distribution/${distributionId}", {
+                distributionId
+            })
+        });
+        imagesBucket.addToResourcePolicy(bucketPolicy);
+    }
+
+    private createCloudfrontDistribution(zone: IHostedZone) {
         const siteCertificate = new DnsValidatedCertificate(this, "WebsiteCertificate", {
-            domainName: `${subdomain}.${apexDomain}`,
+            domainName: `${this.subdomain}.${this.apexDomain}`,
             hostedZone: zone,
             region: "us-east-1"  //standard for acm certs
         });
@@ -67,8 +126,8 @@ export class OptimizedImageDeliveryStack extends cdk.Stack {
             } as IOrigin, // because the S3Origin construct doesn't support Origin Access Control yet
         };
 
-        const distribution = new Distribution(this, "SiteDistribution", {
-            domainNames: [`${subdomain}.${apexDomain}`],
+        return new Distribution(this, "SiteDistribution", {
+            domainNames: [`${this.subdomain}.${this.apexDomain}`],
             certificate: siteCertificate,
             errorResponses: [
                 {
@@ -104,12 +163,12 @@ export class OptimizedImageDeliveryStack extends cdk.Stack {
                             architecture: Architecture.X86_64,
                             timeout: Duration.seconds(7),
                             logRetention: RetentionDays.FIVE_DAYS,
-                            functionName: `optimized-img-delivery-ori-req-${stackId}`,
+                            functionName: `optimized-img-delivery-ori-req-${this.stack}`,
                             entry: path.join(__dirname, 'lambda@edge', 'origin.request.js'),
                             handler: 'handler',
                             memorySize: 128,
                             role: new Role(this, 'OptimizedImgDeliveryOriReqRole', {
-                                roleName: `optimized-img-delivery-ori-req-${stackId}`,
+                                roleName: `optimized-img-delivery-ori-req-${this.stack}`,
                                 assumedBy: new CompositePrincipal(new ServicePrincipal('edgelambda.amazonaws.com'), new ServicePrincipal('lambda.amazonaws.com')),
                                 managedPolicies: [
                                     ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
@@ -121,54 +180,20 @@ export class OptimizedImageDeliveryStack extends cdk.Stack {
                 ]
             },
         });
+    }
 
-        const bucketPolicy = new PolicyStatement();
-        const {distributionId} = distribution;
-        bucketPolicy.addActions('s3:GetObject');
-        bucketPolicy.addServicePrincipal('cloudfront.amazonaws.com');
-        bucketPolicy.addResources(`${imageBucket.bucketArn}/*`);
-        bucketPolicy.addCondition('StringEquals', {
-            'AWS:SourceArn': Fn.sub("arn:aws:cloudfront::${AWS::AccountId}:distribution/${distributionId}", {
-                distributionId
-            })
-        });
-        imageBucket.addToResourcePolicy(bucketPolicy);
-
-        // Need to use L1 construct for Origin Access Control
-        const originAccessControl = new CfnOriginAccessControl(this, 'DefaultOriginAccessControl', {
-            originAccessControlConfig: {
-                name: `OptimizedImageDelivery-oac-${stackId}`,
-                originAccessControlOriginType: 's3',
-                signingBehavior: 'always',
-                signingProtocol: 'sigv4',
-            },
-        });
-        const cfnDistribution = distribution.node.defaultChild as CfnDistribution;
-        const distributionConfig = cfnDistribution.distributionConfig as any;
-        distributionConfig.origins = [
-            {
-                domainName: imageBucket.bucketRegionalDomainName,
-                id: 'image-bucket',
-                originAccessControlId: originAccessControl.attrId,
-                s3OriginConfig: {}
-            }
-        ];
-        distributionConfig.defaultCacheBehavior.targetOriginId = 'image-bucket';
-
-        const target = RecordTarget.fromAlias(new CloudFrontTarget(distribution));
-        new ARecord(this, 'ARecord', {
-            recordName: `${subdomain}.${apexDomain}`,
-            zone,
-            target,
-            ttl: Duration.hours(1),
-            comment: `Optimized image delivery - '${stackId}'`,
-        });
-        new AaaaRecord(this, 'AAAARecord', {
-            recordName: `${subdomain}.${apexDomain}`,
-            zone,
-            target,
-            ttl: Duration.hours(1),
-            comment: `Optimized image delivery - '${stackId}'`,
+    private imagesBucket() {
+        return new Bucket(this, "ImagesBucket", {
+            bucketName: `optimizedimagedelivery-a79b75-${this.stack}`,
+            cors: [
+                {
+                    allowedMethods: [HttpMethods.GET, HttpMethods.HEAD],
+                    allowedOrigins: ['*'],
+                    allowedHeaders: ['*'],
+                }
+            ],
+            versioned: false,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
         });
     }
 }
